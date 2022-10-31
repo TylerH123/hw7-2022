874bc1c34bfa2be106a335b9f400d244af739054
Tyler <tylhuang@umich.edu>
2022-10-31 12:30:43 -0400

    push

--- js/video.js
+++ js/video.js
@@ -28,3 +28,21 @@ document.querySelector("#faster").addEventListener("click", function() {
 	video.playbackRate *= 1.11111; 
 	console.log(video.playbackRate); 
 });
+
+document.querySelector("#skip").addEventListener("click", function() {
+	let curTime = video.currentTime; 
+	if (curTime + 10 > video.duration) 
+		video.currentTime = 0; 
+	else {
+		video.currentTime += 10; 
+	}
+	console.log(video.currentTime); 
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) 
+		video.muted = false; 
+	else {
+		video.muted = true; 
+	}
+});
